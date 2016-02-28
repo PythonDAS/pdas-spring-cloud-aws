4f8a6d17f1af493bec290957586c000778ac64a9
devarchi33 <devarchi33@gmail.com>
2016-02-28 22:25:08 +0900

    init project.

---
+++ README.md
@@ -0,0 +1,46 @@
+[![axisj-contributed](https://img.shields.io/badge/AXISJ.com-Contributed-green.svg)](https://github.com/axisj) ![](https://img.shields.io/badge/Tulum-Brant&Tiffy-red.svg)
+
+
+Spring-Boot based Amazon Simple Storage Service(S3) API Sample
+=======
+
+
+
+```
+Spring Boot + Spring Cloud AWS
+```
+
+###Description
+http://brantiffy.axisj.com/archives/131
+
+###How to Open
+```
+IntelliJ -> Open -> build.gradle
+```
+
+###Compile Setting
+```
+Open IntelliJ Preference
+    - Build, Execution, Deployment -> Compiler
+        -> Check 'Make project automatically'
+```
+
+###How to Run
+```
+- application-example.properties rename to application.properties
+- set your AWS accessKey & secretKey
+- Run main() on Application Class
+```
+
+### API 
+```
+GET /api/aws/s3/list : List of Objects
+GET /api/aws/s3/download?key={key} : Download
+POST /api/aws/s3/upload : Upload
+```
+
+### Environment
+- Java 8
+- Spring Boot 1.3.0.M2
+- Spring Cloud AWS 1.0.2.RELEASE
+- Gradle 2.4
---
+++ src/main/java/com/pythondas/spring/cloud/aws/AWSConfiguration.java
@@ -0,0 +1,56 @@
+package com.pythondas.spring.cloud.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@PropertySource(value = {"classpath:application.properties", "classpath:aws.properties"})
+public class AWSConfiguration extends WebMvcConfigurerAdapter {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region}")
+    private String region;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Bean
+    public BasicAWSCredentials basicAWSCredentials() {
+        return new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AmazonS3Client amazonS3Client(AWSCredentials awsCredentials) {
+        AmazonS3Client amazonS3Client = new AmazonS3Client(awsCredentials);
+        amazonS3Client.setRegion(Region.getRegion(Regions.fromName(region)));
+        return amazonS3Client;
+    }
+}
